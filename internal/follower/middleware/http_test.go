882d11405befc36481cb7991f8ee0b1816434d17
Quang Thien Nguyen <q.thien.nguyen@outlook.de>
2026-02-14 17:31:21 +0100

    Implement Follower UDP endpoint

---
+++ internal/follower/middleware/http_test.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func StartMockHttpServer(t *testing.T) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		if request.Method != "POST" {
+			t.Errorf("Expected POST request, got %s", request.Method)
+		}
+
+		if request.Header.Get("Content-Type") != "application/json" {
+			t.Errorf("Expected application/json header, got %s", request.Header.Get("Content-Type"))
+		}
+
+		writer.WriteHeader(http.StatusNoContent)
+	}))
+}
+
+func TestPost(t *testing.T) {
+	server := StartMockHttpServer(t)
+	defer server.Close()
+
+	testData := map[string]string{"url": "TestUrl1:8080"}
+	err := post(testData, server.URL)
+	if err != nil {
+		t.Errorf("Expected no error, but got %v", err)
+	}
+}
