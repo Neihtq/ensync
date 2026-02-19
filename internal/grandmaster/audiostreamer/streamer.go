f719f57de532e4434627d9920842799ee9904366
Quang Thien Nguyen <thienlon@Quangs-MacBook-Air.local>
2026-02-19 22:53:44 +0100

    Implement Grandmaster audio streamer

---
+++ internal/grandmaster/audiostreamer/streamer.go
@@ -0,0 +1,64 @@
+// package audiostreamer implements streaming capabilities of audio data
+package audiostreamer
+
+import (
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"ensync/internal/grandmaster/logging"
+	"ensync/internal/grandmaster/subscription"
+)
+
+const (
+	interval  = 20 * time.Millisecond
+	logPrefix = "[AudioStreamer]"
+)
+
+type AudioStreamer struct {
+	Subs *subscription.Subscribers
+}
+
+func streamAudioToFollower(buffer []byte, url string) {
+	logging.Log(logPrefix, "Send audio")
+	addr, err := net.ResolveUDPAddr("udp", url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	logging.Log(logPrefix, "Send audio packaget to address: "+addr.String())
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	conn.Write(buffer)
+}
+
+func StreamAudioToAll(buffer []byte, urls []string) {
+	for _, url := range urls {
+		streamAudioToFollower(buffer, url)
+	}
+}
+
+func (streamer *AudioStreamer) StreamAudioToAllLoop(filePath string) {
+	audioSource, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer audioSource.Close()
+
+	ticker := time.NewTicker(interval)
+	buffer := make([]byte, 3528)
+
+	for range ticker.C {
+		n, err := audioSource.Read(buffer)
+		if n == 0 || err != nil {
+			break
+		}
+
+		StreamAudioToAll(buffer[:n], streamer.Subs.Urls)
+	}
+}
---
+++ internal/grandmaster/audiostreamer/streamer_test.go
@@ -0,0 +1,33 @@
+package audiostreamer
+
+import (
+	"net"
+	"testing"
+)
+
+func TestStreamAutoToAllLoop(t *testing.T) {
+	addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+
+	serverAddr := conn.LocalAddr().String()
+	received := make(chan string)
+	go func() {
+		buf := make([]byte, 1024)
+		n, _, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			return
+		}
+		received <- string(buf[:n])
+	}()
+
+	urls := []string{serverAddr}
+	mockAudioBuffer := []byte{1, 2, 3}
+	StreamAudioToAll(mockAudioBuffer, urls)
+}
