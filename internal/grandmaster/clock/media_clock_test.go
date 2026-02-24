3b7c657957925c06f4b32d9c895c0d8fcd4e6354
Quang Thien Nguyen <q.thien.nguyen@outlook.de>
2026-02-25 00:18:32 +0100

    Fix bug of endless loop

--- internal/grandmaster/clock/media_clock_test.go
+++ internal/grandmaster/clock/media_clock_test.go
@@ -32,3 +32,59 @@ func TestStampTime(t *testing.T) {
 		t.Fatalf("UpdateMediaTime failed: Expected to be greater than %d but was %d", expected, timeStamp)
 	}
 }
+
+func TestUpdateStartTime(t *testing.T) {
+	// arrange
+	mediaClock := NewMediaClock()
+
+	// act
+	mediaClock.UpdateStartTime()
+
+	// assert
+	currTime := time.Now()
+	threshold := 10
+	diff := mediaClock.StartTime.Sub(currTime)
+	if int(diff.Nanoseconds()) >= threshold {
+		t.Fatalf("UpdateStartTime failed: Expected diff to be less than %d but was %d", threshold, diff)
+	}
+}
+
+func TestGetSentTimeInt64(t *testing.T) {
+	// arrange
+	mediaClock := NewMediaClock()
+
+	// act
+	sentTime := mediaClock.GetSentTimeInt64()
+
+	// assert
+	if sentTime != 0 {
+		t.Fatalf("GetSentTimeInt64 failed: Expected 0 but received %d", sentTime)
+	}
+}
+
+func TestAddToSentTime(t *testing.T) {
+	// arrange
+	mediaClock := NewMediaClock()
+
+	// act
+	mediaClock.AddToSentTime(10)
+
+	// assert
+	sentTime := mediaClock.GetSentTimeInt64()
+	if sentTime != 10 {
+		t.Fatalf("AddToSentTime failed: Expected result to be 10 but received %d", sentTime)
+	}
+}
+
+func TestMediaTimeInt64(t *testing.T) {
+	// arrange
+	mediaClock := NewMediaClock()
+
+	// act
+	mediaTime := mediaClock.GetMediaTimeInt64()
+
+	// assert
+	if mediaTime != 0 {
+		t.Fatalf("GetMediaTime64 failed: Expected 0 but received %d", mediaTime)
+	}
+}
