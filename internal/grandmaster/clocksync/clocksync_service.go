174f65fe7fb3c53607da7849378bbd649d9ab555
Quang Thien Nguyen <q.thien.nguyen@outlook.de>
2026-02-27 02:45:18 +0100

    Implement GrandMaster NTP service

--- internal/grandmaster/clocksync/clocksync_service.go
+++ internal/grandmaster/clocksync/clocksync_service.go
@@ -1,1 +1,56 @@
+// Package clocksync server implements a server which the follower use to sync their time via NTP
 package clocksync
+
+import (
+	"encoding/binary"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+const bufferSize = 8
+
+func ExposeNTP(url string, stop chan struct{}) error {
+	addr, err := net.ResolveUDPAddr("udp", url)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	defer conn.Close()
+
+	fmt.Println("Clock Sync Service listening on ", url)
+
+	buffer := make([]byte, bufferSize)
+	for {
+		select {
+		case <-stop:
+			return nil
+		default:
+			numBytes, sender, err := conn.ReadFromUDP(buffer)
+			receivedTime := time.Now().UnixNano()
+
+			if err != nil {
+				fmt.Println("Error reading: ", err)
+				continue
+			}
+			timestamp := binary.BigEndian.Uint64(buffer[:numBytes])
+
+			packet := make([]byte, 24)
+			binary.BigEndian.PutUint64(packet[:8], uint64(timestamp))
+			binary.BigEndian.PutUint64(packet[8:16], uint64(receivedTime))
+			binary.BigEndian.PutUint64(packet[16:], uint64(time.Now().UnixNano())) // sendTime
+
+			_, err = conn.WriteToUDP(packet, sender)
+			if err != nil {
+				fmt.Println("Error sending response:", err)
+			}
+		}
+	}
+}
---
+++ internal/grandmaster/clocksync/clocksync_service_test.go
@@ -0,0 +1,68 @@
+package clocksync
+
+import (
+	"encoding/binary"
+	"net"
+	"testing"
+	"time"
+)
+
+func TestExposeNTP(t *testing.T) {
+	// arrange
+	stop := make(chan struct{})
+	serverAddrStr := "127.0.0.1:10123"
+
+	// ct
+	go func() {
+		if err := ExposeNTP(serverAddrStr, stop); err != nil {
+			t.Errorf("ExposeNTP failed: Server exited with error: %v", err)
+		}
+	}()
+	time.Sleep(100 * time.Millisecond)
+
+	clientConn, err := net.Dial("udp", serverAddrStr)
+	if err != nil {
+		t.Fatalf("ExposeNTP failed: %v", err)
+	}
+	defer clientConn.Close()
+
+	t1 := uint64(time.Now().UnixNano())
+	sendBuffer := make([]byte, 8)
+	binary.BigEndian.PutUint64(sendBuffer, t1)
+
+	_, err = clientConn.Write(sendBuffer)
+	if err != nil {
+		t.Fatalf("Failed to send packet: %v", err)
+	}
+
+	recvBuffer := make([]byte, 24)
+	clientConn.SetReadDeadline(time.Now().Add(2 * time.Second))
+	n, err := clientConn.Read(recvBuffer)
+	if err != nil {
+		t.Fatalf("Failed to read response: %v", err)
+	}
+
+	if n != 24 {
+		t.Errorf("Expected 24 bytes, got %d", n)
+	}
+
+	// assert
+
+	returnedT1 := binary.BigEndian.Uint64(recvBuffer[:8])
+	t2 := binary.BigEndian.Uint64(recvBuffer[8:16])
+	t3 := binary.BigEndian.Uint64(recvBuffer[16:24])
+
+	if returnedT1 != t1 {
+		t.Errorf("Original timestamp mismatch. Sent %d, got %d", t1, returnedT1)
+	}
+
+	if t2 == 0 || t3 == 0 {
+		t.Error("Server timestamps (T2/T3) should not be zero")
+	}
+
+	if t3 < t2 {
+		t.Error("Server Transmit time (T3) cannot be before Receive time (T2)")
+	}
+
+	close(stop)
+}
