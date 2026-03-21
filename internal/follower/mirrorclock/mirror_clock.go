14c0031b8ca4e2e838d01a7be15d528acd786641
Quang Thien Nguyen <thienlon@Quangs-MacBook-Air.local>
2026-03-21 22:55:13 +0100

    Tighten Clock delay threshold to 2ms

--- internal/follower/mirrorclock/mirror_clock.go
+++ internal/follower/mirrorclock/mirror_clock.go
@@ -55,7 +55,7 @@ func (clock *MirrorClock) SyncTime(timeStamps []int64) {
 func (clock *MirrorClock) UpdateOffset(measuredOffset int64, delay int64) {
 	clock.mu.Lock()
 	defer clock.mu.Unlock()
-	if delay > 5_000_000 {
+	if delay > 2_000_000 {
 		return
 	}
 
